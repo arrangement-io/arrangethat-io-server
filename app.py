235ee4507402215e137957a5ec3c476f6798e4ce
Jeffchiucp <jeffchiu2000@gmail.com>
2018-09-23 23:30:18 -0700

    add the templates and static files

--- app.py
+++ app.py
@@ -7,17 +7,19 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def home_page():
-    online_users = mongo.db.users.find({"online": True})
-    return render_template("index.html",
-        online_users=online_users)
+	return "Arrange That!"
+    # user = mongo.db.users.find_one({"_id": user_id})
+    # return render_template("index.html",
+    #     online_users=online_users)
 
 @app.route("/arrangement", methods=['GET'])
 def user_arrangement():
 	arrangement_id = request.args.get("id")
-	if null:
-		# 
+
+	if arrangement_id is null:
+		pass
 		# return all of the arrangement of that user
-		return mongo.db.arrangement.find({"arrangement_id"})
+		#return mongo.db.arrangement.find({"arrangement_id"})
 	else:
 		return arrangement_id
 
@@ -25,11 +27,11 @@ def user_arrangement():
 # @app.route("/arrangement/id")
 
 
-@app.route("/user/<username>")
-def user_profile(username):
-    user = mongo.db.users.find_one_or_404({"_id": username})
-    return render_template("user.html",
-        user=user)
+# @app.route("/user/<username>")
+# def user_profile(username):
+#     user = mongo.db.users.find_one_or_404({"_id": username})
+#     return render_template("index.html",
+#         user=user)
 
 if __name__ == '__main__':
     app.run()
