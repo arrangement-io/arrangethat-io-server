5aa85c172860c56ca10fd9234af2ef2d38dff566
Jeff Chiu <jeffandchiu@gmail.com>
2018-10-02 16:36:08 -0500

    Update test_mongo_data.py
    @deonjo 
    
    I remove the unnecessary methods. For example, remove the old method that are used at app.py
    add_item
    add_container
    add_snapshots
    
    But we can remove it.  and remove the unnecessary comments

--- test_mongo_data.py
+++ test_mongo_data.py
@@ -6,7 +6,6 @@ from config import *
 class Mdb:
     def __init__(self):
         conn_str = "mongodb://%s:%s@%s:%d/%s" % (DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
-        # conn_str = 'mongodb://arrangement_user:arrangement_9pass@ds115963.mlab.com:15963/arrangement_db'
         client = MongoClient(conn_str)
         self.db = client[DB_NAME]\
 
@@ -27,7 +26,7 @@ class Mdb:
             collection = self.db["arrangement"]
             result = collection.find({})
             ret = []
-            for data in result:
+            for arrangement in result:
                 ret.append(data)
         except Exception as exp:
             print("get_all_arrangement() :: Got exception: %s", exp)
@@ -38,7 +37,7 @@ class Mdb:
     ###########################################
     #    GET SINGLE DATA BY ARRANGEMENT_ID    #
     ###########################################
-    def get_data_by_id(self, id):
+    def get_arrangement_by_id(self, id):
         try:
             collection = self.db["arrangement"]
             result = collection.find({'_id': id})
@@ -46,7 +45,7 @@ class Mdb:
             for data in result:
                 ret.append(data)
         except Exception as exp:
-            print("get_data_by_id() :: Got exception: %s", exp)
+            print("get_arrangement_by_id() :: Got exception: %s", exp)
             print(traceback.format_exc())
         return jsonify({'arrangement': ret})
 
@@ -59,39 +58,6 @@ class Mdb:
         return mongo_collection.find(query).count() > 0
 
     
-    ###########################################
-    #               GET DATA BY NAME          #
-    ###########################################
-    def get_data_by_name(self, query, collection):
-        mongo_collection = self.db[collection]
-        response = mongo_collection.find(query)
-        ret = {}
-        for item in response:
-            _id = item['_id']
-            name = item['name']
-            size = item['size']
-            ret['_id'] = _id
-            ret['name'] = name
-            ret['size'] = size
-        return ret        
-
-
-    def get_snapshot_by_name(self, name):
-        try:
-            response = self.db.snapshot.find({'name': name})
-            ret = {}
-            for item in response:
-                _id = item['_id']
-                name = item['name']
-                snapshot = item['snapshot']
-                ret['_id'] = _id
-                ret['name'] = name
-                ret['snapshot'] = snapshot
-        except Exception as exp:
-            print("get_container_by_name() :: Got exception: %s", exp)
-            print(traceback.format_exc())
-        return ret
-
 
 if __name__ == "__main__":
     mdb = Mdb()
