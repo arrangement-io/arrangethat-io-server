3349652d2c208271c7d46a249d26b0427dadc0ac
Jeffchiucp <jeffchiu2000@gmail.com>
2018-10-03 10:17:05 -0500

    Implement validate_arrangement method inside app.py and fix the MongoDB issue and update the common methods inside the test_mongo_data.py

--- app.py
+++ app.py
@@ -4,45 +4,121 @@ import random
 import datetime
 from test_mongo_data import Mdb
 from config import ITEM, CONTAINER, ARRANGEMENT, SNAPSHOT
+import json
+from bson import ObjectId
 
 app = Flask(__name__)
 mdb = Mdb()
 
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
 def uniqueid():
     seed = random.getrandbits(32)
     while True:
-       yield seed
-       seed += 1
+        yield seed
+        seed += 1
 
 
 @app.route("/")
 def home_page():
     return "Arrange That!"
 
+
 @app.route('/arrangement', methods=['POST'])
 @app.route('/api/v1/arrangement', methods=['POST'])
 def save_arrangement():
     arrangement = request.json
-    # TODO: validate_arrangement(arrangement) 
-    # name = arrangement['name']
-    # query = {'name': name}
-    arrangement_exists = mdb.check_arrangement_exists(arrangement)
-    if arrangement_exists:
-        mdb.replace_arrangement(arrangement)
+    # TODO: validate_arrangement(arrangement)
+    json_data = validate_arrangement(arrangement)
+    if json_data == True:
+        arrangement_exists = mdb.check_arrangement_exists(arrangement)
+        if arrangement_exists:
+            mdb.replace_arrangement(arrangement)
+        else:
+            mdb.add_arrangement(arrangement)
+        return JSONEncoder().encode(arrangement)
+        # return jsonify(arrangement)
     else:
-        mdb.add_arrangement(arrangement)
-    return jsonify(arrangement)
+        return jsonify({'message':'json is not validate'})
+
 
 @app.route('/arrangement', methods=['GET'])
+@app.route('/api/v1/arrangement', methods=['GET'])
 @app.route('/arrangement/<string:arrangement_id>', methods=['GET'])
-@app.route('/api/v1/arrangements', methods=['GET'])
-@app.route('/api/v1/<string:arrangement_id>', methods=['GET'])
+@app.route('/api/v1/arrangement/<string:arrangement_id>', methods=['GET'])
 def get_arrangement(arrangement_id=None):
     if arrangement_id is None:
-        return jsonify(mdb.get_all_arrangement())
+        return JSONEncoder().encode(mdb.get_all_arrangements())
     else:
-        return jsonify(mdb.get_arrangement_by_id(arrangement_id))
+        return mdb.get_arrangement_by_id(arrangement_id)
+
+
+def validate_arrangement(arrangement):
+    try:
+        item_id = ''
+        item_name = ''
+        item_size = ''
+        container_id = ''
+        container_name = ''
+        container_size = ''
+        snapshot_id = ''
+        snapshot_name = ''
+        container_key_id = ''
+        item_value_id = ''
+
+        arrangement_id = arrangement['id']
+        name = arrangement['name']
+        timestamp = arrangement['timestamp']
+        is_deleted = arrangement['is_deleted']
+        items = arrangement['items']
+
+        for item in items:
+            item_id = item['id']
+            item_name = item['name']
+            item_size = item['size']
+            if item_id == "" or item_name == "" or item_size == "":
+                break
+
+        containers = arrangement['containers']
+        for container in containers:
+            container_id = container['id']
+            container_name = container['name']
+            container_size = container['size']
+            if container_id == "" or container_name == "" or container_size == "":
+                break
+
+        snapshots = arrangement['snapshots']
+        for snapshot in snapshots:
+            snapshot_id = snapshot['id']
+            snapshot_name = snapshot['name']
+            snapshot_dict = snapshot['snapshot']
+            if snapshot_id == "" or snapshot_name == "" or snapshot_dict == "":
+                break
+            for key, value in snapshot_dict.items():
+                container_key_id = key
+
+                for item_value_id in value:
+                    if container_key_id == "" or not item_value_id:
+                        break
+
+        if(arrangement_id and name and timestamp and item_id
+                and item_name and item_size and container_id and container_name
+                and container_size and snapshot_id and snapshot_name and container_key_id
+                and item_value_id and not is_deleted):
+
+            return True
+        else:
+            return False
+
+    except Exception as exe:
+        print("validate_arrangement() :: Got exception: %s: ", exe)
+        print(traceback.format_exc())
+        return False
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
--- test_mongo_data.py
+++ test_mongo_data.py
@@ -7,7 +7,7 @@ class Mdb:
     def __init__(self):
         conn_str = "mongodb://%s:%s@%s:%d/%s" % (DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
         client = MongoClient(conn_str)
-        self.db = client[DB_NAME]\
+        self.db = client[DB_NAME]
 
 
     ###########################################
@@ -20,42 +20,42 @@ class Mdb:
     #               Replace Arrangement       #
     ###########################################
     def replace_arrangement(self, arrangement):
-        self.db["arrangement"].replace_one({'id': arrangement['id']}, arrangement)
-
+        self.db["arrangement"].replace_one({"id": arrangement["id"]}, arrangement)
 
     ###########################################
     #              GET ALL ARRANGEMENT DATA   #
     ###########################################
-    def get_all_arrangement(self):
+    def get_all_arrangements(self):
         try:
             return list(self.db["arrangement"].find({}))
-
         except Exception as exp:
-            print("get_all_arrangement() :: Got exception: %s", exp)
+            print("get_all_arrangements() :: Got exception: %s", exp)
             print(traceback.format_exc())
-        return jsonify({'arrangements': ret})
-
+        return jsonify({"error: %s": exp})
 
     ###########################################
     #    GET SINGLE DATA BY ARRANGEMENT_ID    #
     ###########################################
     def get_arrangement_by_id(self, id):
         try:
-            collection = self.db["arrangement"]
-            result = collection.find({'id': id})
-            if len(result) == 1:
-                return jsonify(result[0])
+            result = self.db["arrangement"].find({"id": id})
+            response = []
+            for data in result:
+                response.append(data)
+            if len(response) == 1:
+                return jsonify({"arrangement": response[0]})
             else:
-                return "no arrangement found"
+                return jsonify({"arrangement": "no arrangement found"})
         except Exception as exp:
-            print("get_arrangement_by_id() :: Got exception: %s", exp)
+            print("get_arrangement_by_id():: Got exception: %s", exp)
             print(traceback.format_exc())
+            return jsonify({"error %s: ", exp})
 
     ###########################################
     #         CHECK ARRANGEMENT EXISTS        #
     ###########################################
     def check_arrangement_exists(self, arrangement):
-        return self.db["arrangement"].find({'id':arrangement['id']}).count() > 0
+        return self.db["arrangement"].find({"id": arrangement["id"]}).count() > 0
     
 
 if __name__ == "__main__":
