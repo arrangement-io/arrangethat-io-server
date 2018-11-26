918deed0c724932638f410bdadbfe9cf139aac12
Gideon Chia <gchia@gchia-mn2.linkedin.biz>
2018-11-25 20:53:27 -0800

    Added google sso to backend

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session, redirect, url_for
+from flask_cors import CORS
+from flask_oauth import OAuth
+
+from bson import ObjectId
+import json
+import os
 import traceback
+
+from config import ITEM, CONTAINER, ARRANGEMENT, SNAPSHOT, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, REDIRECT_URI
 from test_mongo_data import Mdb
 from test.data_generator.data_generator import Arrangement
 
-from config import ITEM, CONTAINER, ARRANGEMENT, SNAPSHOT
-import json
-from bson import ObjectId
-from flask_cors import CORS
-
+SECRET_KEY = 'development key'
+DEBUG = True
 
 app = Flask(__name__)
+oauth = OAuth()
 CORS(app)
 mdb = Mdb()
 arrangement_obj = Arrangement()
+
+app.debug = DEBUG
+app.secret_key = SECRET_KEY
+
+google = oauth.remote_app('google',
+                          base_url='https://www.google.com/accounts/',
+                          authorize_url='https://accounts.google.com/o/oauth2/auth',
+                          request_token_url=None,
+                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                                                'response_type': 'code'},
+                          access_token_url='https://accounts.google.com/o/oauth2/token',
+                          access_token_method='POST',
+                          access_token_params={'grant_type': 'authorization_code'},
+                          consumer_key=GOOGLE_CLIENT_ID,
+                          consumer_secret=GOOGLE_CLIENT_SECRET)
+
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
@@ -23,8 +45,43 @@ class JSONEncoder(json.JSONEncoder):
 
 @app.route("/")
 def home_page():
-    return "Arrange That!"
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
 
+    access_token = access_token[0]
+    from urllib2 import Request, urlopen, URLError
+
+    headers = {'Authorization': 'OAuth '+access_token}
+    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+                  None, headers)
+    try:
+        res = urlopen(req)
+    except URLError, e:
+        if e.code == 401:
+            # Unauthorized - bad token
+            session.pop('access_token', None)
+            return redirect(url_for('login'))
+        return res.read()
+
+    return res.read()
+    # return "Arrange That!"
+
+@app.route("/login")
+def login():
+    callback=url_for('authorized', _external=True)
+    return google.authorize(callback=callback)
+
+@app.route(REDIRECT_URI)
+@google.authorized_handler
+def authorized(resp):
+    access_token = resp['access_token']
+    session['access_token'] = access_token, ''
+    return redirect(url_for('index'))
+
+@google.tokengetter
+def get_access_token():
+    return session.get('access_token')
 
 @app.route('/arrangement', methods=['POST'])
 @app.route('/api/v1/arrangement', methods=['POST'])
